9d839a6f62c8b2cea00f2d340c6031d716cdb1b1
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-05-30 13:24:30 +0530

    Initial Commit

---
+++ src/hooks/useMounted.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useMounted = () => {
+    const [mounted, setMounted] = React.useState(false)
+    React.useEffect(() => {
+        setMounted(true)
+    }, [])
+   
+  return mounted
+}
+
+export default useMounted
